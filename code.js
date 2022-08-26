c4938fef5d30b26b8dab9d15c827d0063bb4e6ed
Federico Orsi <federicoantonio.orsi@gmail.com>
2022-08-26 11:41:06 -0300

    ref

--- code.js
+++ code.js
@@ -1,19 +1,3 @@
-const swalConImg = () => {
- 
-  
-  Swal.fire({
-    title: 'Sweet!',
-    text: 'Modal with a custom image.',
-    imageUrl: 'https://unsplash.it/400/200',
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  })
-
-}
-
-swalConImg();
-
 const elementosCarrito = [];
 
 carritoRecuperado = localStorage.getItem("carrito") && JSON.parse(localStorage.getItem("carrito"));
