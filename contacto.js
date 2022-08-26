ae2e5c9a9a750543a195249541e2885531f39cc8
Federico Orsi <federicoantonio.orsi@gmail.com>
2022-08-26 11:55:39 -0300

    refresh

---
+++ contacto.js
@@ -0,0 +1,15 @@
+const swalConImg = () => {
+ 
+  
+    Swal.fire({
+      title: 'Solo falta un paso más!!',
+      text: 'Por favor completa el siguiente formulario antes de realizar el Pago.',
+      imageUrl: '../img/teamwork.jpg',
+      imageWidth: 400,
+      imageHeight: 200,
+      imageAlt: 'Custom image',
+    })
+  
+  }
+  
+  swalConImg();
